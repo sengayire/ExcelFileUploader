85bf9162f1ef2bc76c3cf64653b123d3e2aa18e4
sengayire <psengayire@gmail.com>
2021-08-14 13:45:56 +0200

    refactor codes

--- src/helpers/getUsersFromStream.ts
+++ src/helpers/getUsersFromStream.ts
@@ -1,10 +1,3 @@
-import AWS from 'aws-sdk';
-
-AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-});
-
 const getBuffer = (file, callback) => {
   const buffers = [];
 
